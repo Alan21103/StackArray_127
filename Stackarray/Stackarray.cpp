3c0a190cbb509e2d4313b4a329a3ff084a6d7878
Alan21103 <syahlansaputra03@gmail.com>
2023-06-15 15:45:58 +0700

    Membuat stack 5 array

--- Stackarray/Stackarray.cpp
+++ Stackarray/Stackarray.cpp
@@ -5,15 +5,3 @@ using namespace std;
 class StackArray {
 private:
 	string stack_array[5];
-	int top;
-public:
-	//constructor
-	StackArray() {
-		top = -1;
-	}
-
-	string push(string element) {
-		if (top == 4) {		//step 1
-			cout << "Number of data exceeds the limit. " << endl;
-			return "";
-		}
