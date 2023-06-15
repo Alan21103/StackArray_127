cb9ca45fc75679ccbc5d4db54e995e56fb2e37dd
Alan21103 <syahlansaputra03@gmail.com>
2023-06-15 15:48:44 +0700

    Menambahkan elemen baru kedalam tumpukan dengan cara menaikkan nilai

--- Stackarray/Stackarray.cpp
+++ Stackarray/Stackarray.cpp
@@ -5,3 +5,23 @@ using namespace std;
 class StackArray {
 private:
 	string stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {		//step 1
+			cout << "Number of data exceeds the limit. " << endl;
+			return "";
+		}
+
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " ditambahkan(pushed)" << endl;
+
+		return element;
+	}
